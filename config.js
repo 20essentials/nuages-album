45edc31d53b7030f9efd81e0af90b83420e86dad
DominicDev <s0l0estudi4r@gmail.com>
2026-01-29 16:43:02 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'NUAGES';
+export let nameSongs = [
+	"CLOSER", /*1*/
+	"DISTANT", /*2*/
+	"DREAMS", /*3*/
+	"REST", /*4*/
+	"MISSING", /*5*/
+	"STILL", /*6*/
+	"SOON", /*7*/
+	"HELD", /*8*/
+	"FROM LOVE", /*9*/
+	"CALLING", /*10*/
+	"WAIT", /*11*/
+	"CLAIMS", /*12*/
+	"SHELTER", /*13*/
+	"SHELTER, PT. 2", /*14*/
+	"BESIDE YOU", /*15*/
+	"SOLO", /*16*/
+	"PILLOW TALK", /*17*/
+	"MILKSHAKE", /*18*/
+	"SHY (SLOWED)", /*19*/
+	"LOVE (SLOWED)", /*20*/
+]
